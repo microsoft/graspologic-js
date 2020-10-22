331143f42514ad1edd04ad417058deedf8e963b2
Chris Trevino <darthtrevino@gmail.com>
2020-10-22 00:02:53 -0700

    add missing header

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
 module.exports = {
 	presets: [
-		[require('@babel/preset-env'), { targets: { node: 'current' } }],
-		require('@babel/preset-typescript'),
-		require('@babel/preset-react'),
+		['@babel/preset-env', { targets: { node: 'current' } }],
+		'@babel/preset-typescript',
+		'@babel/preset-react',
 	],
 }
