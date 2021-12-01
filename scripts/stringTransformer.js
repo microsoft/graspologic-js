df75b19d6ae5336bd725db43bc00c809a4c26122
Chris Trevino <chtrevin@microsoft.com>
2021-11-30 17:28:24 -0800

    remove unused script

--- scripts/stringTransformer.js
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) Microsoft. All rights reserved.
- * Licensed under the MIT license. See LICENSE file in the project.
- */
-module.exports = {
-	process(src) {
-		return `module.exports = \`${src}\``
-	},
-}
